4a235618bfee87c964fcd525f4a46e163a89e9f2
Sandeep123451 <144896730+Sandeep123451@users.noreply.github.com>
2024-09-29 17:56:27 +0530

    Add files via upload

---
+++ face recognization attendance/AddDatatoDatabase.py
@@ -0,0 +1,46 @@
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred, {
+    'databaseURL': "https://face-attendance-in-live-default-rtdb.firebaseio.com/"
+})
+
+ref = db.reference('Students')
+
+data = {
+    "22QM1A6630":
+        {
+            "name": "KOTIGARI SANDEEP",
+            "major": "Robotics",
+            "starting_year": 2017,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 4,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "22QM1A6628":
+        {
+            "name": "KELGIRI MARUTHI",
+            "major": "Economics",
+            "starting_year": 2021,
+            "total_attendance": 12,
+            "standing": "B",
+            "year": 1,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        },
+    "23QM1A6692":
+        {
+            "name": "KALYAN",
+            "major": "Physics",
+            "starting_year": 2020,
+            "total_attendance": 7,
+            "standing": "G",
+            "year": 2,
+            "last_attendance_time": "2022-12-11 00:54:34"
+        }
+}
+
+for key, value in data.items():
+    ref.child(key).set(value)
